9d8f0f48cf8a9c0fc2675546c83e9678a79131c4
vespucchi <56511717+vespucchi@users.noreply.github.com>
2024-08-10 18:50:25 +0200

    ADD: router + paths

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,51 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import './main.css';
+import App from './App.jsx';
+import ErrorPage from './pages/Error.jsx';
+import HomePage from './pages/Home.jsx';
+import BrowsePage from './pages/Browse.jsx';
+import GamePage from './pages/Game.jsx';
+import UserPage from './pages/User.jsx';
+import WishlistPage from './pages/Wishlist.jsx';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                index: true,
+                element: <HomePage />
+            },
+            {
+                path: "browse",
+                element: <BrowsePage />
+            },
+            {
+                path: "game/:gameId",
+                element: <GamePage />
+            },
+            {
+                path: "account",
+                element: <UserPage />,
+            },
+            {
+                path: "account/wishlist",
+                element: <WishlistPage />,
+            },
+        ]
+    },
+    
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
