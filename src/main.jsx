97a3884efcfb2de78d5f7819b466b5d8830e6a1d
vespucchi <56511717+vespucchi@users.noreply.github.com>
2024-08-14 20:07:11 +0200

    FIX(main): split routes

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,15 @@ import {
 import './main.css';
 import App from './App.jsx';
 import ErrorPage from './pages/Error.jsx';
-import HomePage from './pages/Home.jsx';
+import DiscoverPage from './pages/Discover.jsx';
 import BrowsePage from './pages/Browse.jsx';
+import CollectionPage from './pages/Collection.jsx';
 import GamePage from './pages/Game.jsx';
-import UserPage from './pages/User.jsx';
+import ProfilePage from './pages/Profile.jsx';
 import WishlistPage from './pages/Wishlist.jsx';
+import CartPage from './pages/Cart.jsx';
+import CheckoutPage from './pages/Checkout.jsx';
+
 
 const router = createBrowserRouter([
     {
@@ -20,28 +24,54 @@ const router = createBrowserRouter([
         errorElement: <ErrorPage />,
         children: [
             {
-                index: true,
-                element: <HomePage />
-            },
-            {
-                path: "browse",
-                element: <BrowsePage />
-            },
-            {
-                path: "game/:gameId",
-                element: <GamePage />
-            },
-            {
-                path: "account",
-                element: <UserPage />,
-            },
-            {
-                path: "account/wishlist",
-                element: <WishlistPage />,
-            },
+                errorElement: <ErrorPage />,
+                children: [
+                    {
+                        index: true,
+                        element: <DiscoverPage />
+                    },
+                    {
+                        path: "browse",
+                        element: <BrowsePage />,
+                    },
+                    {
+                        path: "collection/:title",
+                        element: <CollectionPage />,
+                    },
+                    {
+                        path: "game/:gameId",
+                        element: <GamePage />
+                    },
+                    {
+                        path: "cart",
+                        element: <CartPage />
+                    },
+                    {
+                        path: "wishlist",
+                        element: <WishlistPage />
+                    },
+                ]
+            }
         ]
     },
-    
+    {
+        path: "/profile",
+        element: <ProfilePage />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                errorElement: <ErrorPage />,
+                children: [
+                    // TO DO
+                ]
+            }
+        ]
+    },
+    {
+        path: "/checkout",
+        element: <CheckoutPage />,
+        errorElement: <ErrorPage />,
+    },
 ]);
 
 createRoot(document.getElementById('root')).render(
